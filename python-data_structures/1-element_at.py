cb68c6ee6eb393e466cba9d8cedb2851b8956e09
root <root@ip-10-42-178-228.ec2.internal>
2025-11-25 12:15:33 +0000

    message

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,5 @@
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+ #!/usr/bin/python
+    def element_at(my_list, idx):
+        if idx < 0 or idx >= len(my_list):
+            return None
+        return my_list[idx]
