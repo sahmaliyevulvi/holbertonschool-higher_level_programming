44ef9b670e461bc9632fbc57d7886f29c20b3c2e
root <root@ip-10-42-189-220.ec2.internal>
2025-11-30 12:23:15 +0000

    message

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+It provides width and height validation,
+area and perimeter calculation,
+and string representation using '#'.
+"""
+
+
+class Rectangle:
+    """Defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle.
+        If width or height is 0, return 0.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return the string representation of the rectangle using '#'."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rect = []
+        for _ in range(self.__height):
+            rect.append("#" * self.__width)
+        return "\n".join(rect)
+
+    def __repr__(self):
+        """Return a formal string representation."""
+        return "<{}-{}.{} object at {}>".format(
+            __file__.split('/')[-1].split('.')[0],
+            self.__class__.__name__,
+            self.__class__.__name__.lower(),
+            hex(id(self))
+        )
