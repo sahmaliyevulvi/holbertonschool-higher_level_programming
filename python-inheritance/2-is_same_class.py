620e9b0a961747d0b606d59b014efde6302cdfb0
root <root@ip-10-42-226-170.ec2.internal>
2025-12-01 11:06:24 +0000

    enw

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Function that returns True if an object is exactly an instance of a specified class"""
+"""Function that returns True if an object is exactly an instance
+of a specified class"""
+
 
 def is_same_class(obj, a_class):
     """Return True if obj is exactly an instance of a_class, otherwise False"""
