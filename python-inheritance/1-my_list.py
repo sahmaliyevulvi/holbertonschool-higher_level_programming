a0fc4f89734674f41d969ec89f0fbcd0689bb64c
root <root@ip-10-42-226-170.ec2.internal>
2025-12-01 10:38:59 +0000

    new

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-"""Defines the MyList class that inherits from list."""
+"""Create a class MyList"""
 
 
 class MyList(list):
-    """A class that inherits from list."""
-
-    def __init__(self):
-        self.res = []
-
+    """Class MyList that inherits from list"""
     def print_sorted(self):
-
-        for i in self:
-            if i > 0:
-                self.res.append(i)
-
-        print(sorted(self.res))
+        """Print the list sorted"""
+        print(sorted(self))
