3e54502c4691f2010e6a106da713f0515df03f34
root <root@ip-10-42-226-170.ec2.internal>
2025-12-01 11:14:07 +0000

    enw

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that returns True if the object is an instance of, or inherited from, a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is an instance of a_class or its subclass, otherwise False"""
+    return isinstance(obj, a_class)
