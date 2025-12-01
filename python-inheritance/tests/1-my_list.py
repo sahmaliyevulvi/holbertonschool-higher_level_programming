cefbcf7529769029a631543435c31c3320439d70
root <root@ip-10-42-226-170.ec2.internal>
2025-12-01 09:38:13 +0000

    message

--- python-inheritance/tests/1-my_list.py
+++ python-inheritance/tests/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Defines the MyList class that inherits from list."""
 
+
 class MyList(list):
-    """Custom list class that extends list."""
+    """A class that inherits from list."""
 
     def print_sorted(self):
-        """Prints the list but sorted in ascending order."""
+        """Prints the list in ascending order without modifying the original."""
         print(sorted(self))
