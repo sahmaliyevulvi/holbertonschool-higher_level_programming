84faae7a51d9d73777b0564bef760e7070b2ac4b
root <root@ip-10-42-111-141.ec2.internal>
2025-12-04 09:23:03 +0000

    new

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,8 +13,4 @@ class Square(Rectangle):
 
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
-
-    def area(self):
-        """Returns the area of the square"""
