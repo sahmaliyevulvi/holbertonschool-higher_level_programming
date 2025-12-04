a9352545980ae2175ef00cdcaa8a44fd550b0167
root <root@ip-10-42-111-141.ec2.internal>
2025-12-04 09:19:55 +0000

    new

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Rectangle class that inherits from BaseGeometry
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle with width and height validated by BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """String representation of the rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
