c9f85f9625eb29f9d3b83e1eb6f7d94dd948d4ff
root <root@ip-10-42-111-141.ec2.internal>
2025-12-04 09:21:56 +0000

    new

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Square class that inherits from Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square with size validated by BaseGeometry
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of the square"""
