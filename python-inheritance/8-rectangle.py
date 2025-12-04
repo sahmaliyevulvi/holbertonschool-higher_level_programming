77c573257510e3720ee58da467a355e6617b2c9b
root <root@ip-10-42-111-141.ec2.internal>
2025-12-04 09:16:34 +0000

    new

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Rectangle class that inherits from BaseGeometry
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle with width and height validated by BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
