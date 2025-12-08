67c3ea7a748576ced34bb85bcae0b75d764f7c38
root <root@ip-10-42-49-157.ec2.internal>
2025-12-08 15:31:05 +0000

    new

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that defines a
+function to convert a JSON string to a Python object
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns the Python object
+    represented by a JSON string
+    """
+    return json.loads(my_str)
