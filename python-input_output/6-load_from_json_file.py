c4c66652712f6e363b987d586c5126c079d27ddb
root <root@ip-10-42-49-157.ec2.internal>
2025-12-08 15:32:52 +0000

    new

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that defines a function to load
+a Python object from a JSON file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Creates a Python object from a JSON file"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
